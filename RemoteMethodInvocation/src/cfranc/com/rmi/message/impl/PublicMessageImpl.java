a76c579775b86c099b10b990c7903f921622eabb
dimitry.krychylskyy <dimitry.krychylskyy@FR-L3356885.groupinfra.com>
2018-04-10 12:59:58 +0200

    Creation l'inerface

--- RemoteMethodInvocation/src/cfranc/com/rmi/message/impl/PublicMessageImpl.java
+++ RemoteMethodInvocation/src/cfranc/com/rmi/message/impl/PublicMessageImpl.java
@@ -1,5 +1,34 @@
 package cfranc.com.rmi.message.impl;
 
-public class PublicMessageImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import cfranc.com.rmi.message.PublicMessage;
+
+public class PublicMessageImpl extends UnicastRemoteObject implements PublicMessage {
+
+	private static final long serialVersionUID = 8086750426071332539L;
+	private String text = null;
+
+	protected PublicMessageImpl() throws RemoteException {
+		super();
+	}
+
+	public PublicMessageImpl(String text) throws RemoteException {
+		super();
+		this.text = text;
+	}
+
+	@Override
+	public String getText() throws RemoteException {
+		// TODO Auto-generated method stub
+		return text;
+	}
+
+	@Override
+	public void setText(String text) throws RemoteException {
+		text = "t";
+
+	}
 
 }
